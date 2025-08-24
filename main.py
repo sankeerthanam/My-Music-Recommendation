f915ec64d318092d6742569e4619b161a0eb42aa
sankeerthanam <82747723+sankeerthanam@users.noreply.github.com>
2025-08-24 01:34:59 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+# app.py
+
+import streamlit as st
+from recommend import df, recommend_songs
+
+# Set custom Streamlit page config
+st.set_page_config(
+    page_title="Music Recommender 🎵",
+    page_icon="🎧",  
+    layout="centered"
+)
+
+
+st.title("🎶Your Music Recommender")
+
+song_list = sorted(df['song'].dropna().unique())
+selected_song = st.selectbox("🎵 Select a song:", song_list)
+
+if st.button("Recommend Similar Songs"):
+    with st.spinner("Finding similar songs..."):
+        recommendations = recommend_songs(selected_song)
+        if recommendations is None:
+            st.warning("Sorry, song not found.")
+        else:
+            st.success("Top similar songs:")
+            st.table(recommendations)
